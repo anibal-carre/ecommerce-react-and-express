fd1af7fed8c6b92faac6c1e57841111c3b45dc8b
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2023-11-27 19:47:46 -0300

    register and login added

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
 const usersDb = require("./db/db");
 
 const app = express();
 app.use(express.json());
+app.use(morgan("dev"));
 
 // USERS LIST
 app.get("/api/users", (req, res) => {
@@ -27,6 +29,31 @@ app.post("/api/register", (req, res) => {
   const newUser = { id: usersDb.length + 1, ...req.body };
   usersDb.push(newUser);
   res.json(newUser);
+});
+
+// LOGIN ROUTE
+
+app.post("/api/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const foundUser = usersDb.find(
+    (user) =>
+      user.account.username == username && user.account.password == password
+  );
+
+  if (!foundUser) {
+    return res.status(404).json({
+      message: "User not found",
+    });
+  }
+  res.json({
+    Status: "Login Successsfully",
+    message: {
+      Username: username,
+      Password: password,
+    },
+  });
 });
 
 app.listen(3000);
