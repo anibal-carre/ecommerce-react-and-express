4896f08b864364b4e4c5fad21e38eb1e6d7f571d
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2023-12-12 20:43:11 -0300

    products card

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,11 +1,46 @@
-import AsideSection from "./AsideSection";
 import BannerSection from "./BannerSection";
+import { useState, useEffect } from "react";
 
 const HeroSection = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/products")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
+  console.log(data);
   return (
-    <section className="border border-black w-full h-screen p-4 md:p-8 flex justify-center gap-10">
-      <AsideSection />
-      <BannerSection />
+    <section className="border border-black w-full h-auto min-h-screen p-4 md:p-8 flex flex-col items-center">
+      <div className="max-w-[1800px] w-full h-full flex gap-10 justify-center mb-20">
+        <BannerSection />
+      </div>
+
+      <div className="w-full max-w-[1300px] h-auto flex justify-evenly p-5 mb-10">
+        <div className="border w-[260px] h-[320px] rounded-[2px] shadow-sm shadow-black cursor-pointer hover:scale-105 transition-all delay-75">
+          <span>{data?.Iphone[0].name}</span>
+        </div>
+        <div className="border w-[260px] h-[320px] rounded-[2px] shadow-sm shadow-black cursor-pointer hover:scale-105 transition-all delay-75">
+          <span>{data?.Samsung[0].name}</span>
+        </div>
+        <div className="border w-[260px] h-[320px] rounded-[2px] shadow-sm shadow-black cursor-pointer hover:scale-105 transition-all delay-75">
+          <span>{data?.Samsung[1].name}</span>
+        </div>
+        <div className="border w-[260px] h-[320px] rounded-[2px] shadow-sm shadow-black cursor-pointer hover:scale-105 transition-all delay-75"></div>
+      </div>
+      <div className="w-full max-w-[1300px] h-auto flex justify-evenly p-5 mb-10">
+        <div className="border w-[260px] h-[320px] rounded-[2px] shadow-sm shadow-black cursor-pointer hover:scale-105 transition-all delay-75"></div>
+        <div className="border w-[260px] h-[320px] rounded-[2px] shadow-sm shadow-black cursor-pointer hover:scale-105 transition-all delay-75"></div>
+        <div className="border w-[260px] h-[320px] rounded-[2px] shadow-sm shadow-black cursor-pointer hover:scale-105 transition-all delay-75"></div>
+        <div className="border w-[260px] h-[320px] rounded-[2px] shadow-sm shadow-black cursor-pointer hover:scale-105 transition-all delay-75"></div>
+      </div>
+      <div className="w-full max-w-[1300px] h-auto flex justify-evenly p-5 mb-10">
+        <div className="border w-[260px] h-[320px] rounded-[2px] shadow-sm shadow-black cursor-pointer hover:scale-105 transition-all delay-75"></div>
+        <div className="border w-[260px] h-[320px] rounded-[2px] shadow-sm shadow-black cursor-pointer hover:scale-105 transition-all delay-75"></div>
+        <div className="border w-[260px] h-[320px] rounded-[2px] shadow-sm shadow-black cursor-pointer hover:scale-105 transition-all delay-75"></div>
+        <div className="border w-[260px] h-[320px] rounded-[2px] shadow-sm shadow-black cursor-pointer hover:scale-105 transition-all delay-75"></div>
+      </div>
     </section>
   );
 };
--- client/src/components/Home/HomeNav.jsx
+++ client/src/components/Home/HomeNav.jsx
@@ -1,11 +1,16 @@
 const HomeNav = () => {
   return (
     <nav className="w-full max-w-[1100px] h-20 flex justify-between border border-black">
-      <div className="w-[20%] border border-blue-500"></div>
+      <div className="w-[20%] flex justify-center items-center border border-blue-500">
+        <span className="font-bold text-[20px]">PHONE SHOP</span>
+      </div>
       <div className="w-[45%] flex items-center border border-blue-500">
         <input className="w-[90%] h-12 border" type="text" />
       </div>
-      <div className="w-[25%] border border-blue-500"></div>
+      <div className="w-[25%] flex items-center justify-center gap-8 border border-blue-500">
+        <span className="font-bold">My Account</span>
+        <span>Cart</span>
+      </div>
     </nav>
   );
 };
