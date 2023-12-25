950bc6224964dff7b303d7b9702999cd47e2de9c
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2023-12-25 13:24:46 -0300

    new card

--- client/src/components/BannerSection.jsx
+++ client/src/components/BannerSection.jsx
@@ -1,10 +1,10 @@
 const BannerSection = () => {
   return (
-    <div className="w-[90%]">
+    <div className="w-[90%] h-auto max-h-[350px]">
       <h2 className="font-bold text-[20px] mb-5">Black Friday</h2>
       <div className="w-full h-[300px]">
         <img
-          className="w-full"
+          className="w-full h-auto max-h-[350px]"
           src="https://trocafone.vtexassets.com/assets/vtex.file-manager-graphql/images/1b64146c-8cb8-48ba-a1df-3a670a364349___0219c8f95dc82c2040fa0bc0361242f2.png"
           alt="banner"
         />
--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,6 +1,6 @@
 const ProductCard = ({ data }) => {
   return (
-    <div className="border flex flex-col items-center w-[180px] h-[320px] rounded-[2px] shadow-sm shadow-black cursor-pointer hover:scale-105 transition-all delay-75 p-4">
+    <div className="border flex flex-col items-center w-[180px] h-[370px] rounded-[2px] shadow-sm shadow-black cursor-pointer hover:scale-105 transition-all delay-75 p-4">
       <div className="flex justify-center items-center">
         <img
           className="w-[127px] h-[150px]"
@@ -13,9 +13,15 @@ const ProductCard = ({ data }) => {
         <span className="text-[13px] font-[500] mb-2">{data?.name}</span>
 
         <span className="text-[13px] text-zinc-400">A partir de</span>
-        <span className="text-[19px] font-[700] text-[#ff8800]">
+        <span className="text-[19px] font-[700] text-[#ff8800] mb-4">
           {`R$ ${data?.price.toLocaleString()},00`}
         </span>
+
+        <span className="text-[10px]">{`12x de R$ ${(data?.price / 12).toFixed(
+          2
+        )} sem juros ou no pix 10% off R$ ${(
+          data?.price - 100.44
+        ).toLocaleString()}`}</span>
       </div>
     </div>
   );
